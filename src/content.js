cf24cc750844492f2389882c4a09aa628e862fb4
dpliakos <dimitrispl96@gmail.com>
2018-07-21 22:42:54 +0300

    add sample content

---
+++ src/content.js
@@ -0,0 +1,40 @@
+const content = [
+  {
+    title: 'this is a title 1',
+    link: 'https://google.com',
+    body: `Lorem ipsum dolor sit amet, cu vide propriae interpretaris sed,
+    posse placerat dissentiet ut ius. Ei quo graecis elaboraret. Ex eum legere
+    docendi cotidieque. Sed ei tota fabulas vivendo, justo molestie vim in.
+    Sit ut sale nonumes legendos, dicam homero soluta at mei.`,
+    icon: '/src/images/sample.svg'
+  },
+  {
+    title: 'this is a title 2',
+    link: 'https://google.com',
+    body: `Lorem ipsum dolor sit amet, cu vide propriae interpretaris sed,
+    posse placerat dissentiet ut ius. Ei quo graecis elaboraret. Ex eum legere
+    docendi cotidieque. Sed ei tota fabulas vivendo, justo molestie vim in.
+    Sit ut sale nonumes legendos, dicam homero soluta at mei.`,
+    icon: '/src/images/sample.svg'
+  },
+  {
+    title: 'this is a title 3',
+    link: 'https://google.com',
+    body: `Lorem ipsum dolor sit amet, cu vide propriae interpretaris sed,
+    posse placerat dissentiet ut ius. Ei quo graecis elaboraret. Ex eum legere
+    docendi cotidieque. Sed ei tota fabulas vivendo, justo molestie vim in.
+    Sit ut sale nonumes legendos, dicam homero soluta at mei.`,
+    icon: '/src/images/sample.svg'
+  },
+  {
+    title: 'this is a title 4',
+    link: 'https://google.com',
+    body: `Lorem ipsum dolor sit amet, cu vide propriae interpretaris sed,
+    posse placerat dissentiet ut ius. Ei quo graecis elaboraret. Ex eum legere
+    docendi cotidieque. Sed ei tota fabulas vivendo, justo molestie vim in.
+    Sit ut sale nonumes legendos, dicam homero soluta at mei.`,
+    icon: '/src/images/sample.svg'
+  }
+]
+
+export default content;
