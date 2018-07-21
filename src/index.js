dbb8a504d51de56b835dc1c1bfa7cf74f73b58f5
dpliakos <dimitrispl96@gmail.com>
2018-07-21 21:35:23 +0300

    initialize repo

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import App from './containers/app'
+import './css/styles/all.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
