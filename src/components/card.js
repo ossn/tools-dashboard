c22df81a8c27939aa75b611f011296c9401456a7
dpliakos <dimitrispl96@gmail.com>
2018-07-21 22:43:12 +0300

    import card component

---
+++ src/components/card.js
@@ -0,0 +1,57 @@
+import React from 'react';
+/*
+  props
+  title: string, the card's title
+  link: String, card's link
+  body: string, card's inner text
+  footerPicture:  string picture's path
+  j
+*/
+export default class Card extends React.Component  {
+  render() {
+
+    const title = this.props.title || '';
+    const card_link = this.props.link || '#';
+    let body = this.props.body || '';
+
+
+    let  link = <div></div>;
+    if (this.props.link) {
+      link = <div className="link card__link-wrapper">
+               <a className="link card__link" href={card_link}> {card_link} </a>
+             </div>
+    }
+
+
+    let footerPicture = <div></div>;
+    if (this.props.footerPicture) {
+      const bundle = this.props.footerPicture;
+      footerPicture =
+        <img
+          src={bundle.drc}
+          alt={bundle.alt || ''}
+          className="card__footer-picture"
+        />
+    }
+
+
+
+
+    return(
+      <div className="card">
+        <div className="card__header">
+          <div className="title card__title"> {title} </div>
+          {link}
+        </div>
+        <div className="card__body">
+          {body}
+        </div>
+        <div className="card__footer">
+          <div className="card__footer-picture-wrapper">
+            {footerPicture}
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
