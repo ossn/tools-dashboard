96ad53e9478186bbd41580e8105b19a2f94e69bc
dpliakos <dimitrispl96@gmail.com>
2018-07-21 22:43:12 +0300

    import card component

--- src/containers/app.js
+++ src/containers/app.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import Card from './../components/card';
+import contentData from './../content';
+
 
 export default class App extends React.Component  {
   render() {
+
+    const content = contentData.map((item, i)=>{
+
+      return (
+        <Card title={item.title} link={item.link} body={item.body} picture={item.icon} key={i}/>
+      );
+    });
+
+
     return(
-      <div></div>
+      <div>
+        {content}
+      </div>
     );
   }
 }
