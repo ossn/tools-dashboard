68f31ab42889ad0212afe2a5744af2ed92b5d1ee
dpliakos <dimitrispl96@gmail.com>
2018-07-22 03:35:14 +0300

    theme background gradient

--- src/containers/app.js
+++ src/containers/app.js
@@ -7,7 +7,7 @@ import Footer from './footer';
 export default class App extends React.Component  {
   render() {
     return(
-      <div>
+      <div className="app">
         <NavigationBar />
         <ToolsDashboard />
         <Footer />
