c1de230b7f29e378afd16892e1cbfb0426cab5bf
dpliakos <dimitrispl96@gmail.com>
2018-07-23 21:47:15 +0300

    implement search

--- src/components/search-tool.js
+++ src/components/search-tool.js
@@ -2,24 +2,16 @@ import React from 'react';
 
 export default class SearchTool extends React.Component  {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      searchString: '',
-    };
-  }
-
-  updateStr = (event)=>{this.setState({searchString: event.target.value})}
-
-
   render() {
     return(
       <div className="search-tool__wrapper">
         <input type="text" placeholder="Search tool" className="search-tool"
-          value={this.state.searchString || ''} onChange={this.updateStr}/>
-          <div className="search-tool__icon">
-            <span className="lnr lnr-magnifier"></span>
-          </div>
+          value={this.props.searchString || ''}
+          onChange={(event)=>this.props.updateSearch(event)}
+         />
+        <div className="search-tool__icon">
+          <span className="lnr lnr-magnifier"></span>
+        </div>
       </div>
     );
   }
